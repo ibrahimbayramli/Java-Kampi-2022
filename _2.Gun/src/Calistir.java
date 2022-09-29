73730bb4e03efbec3abc0d0af1c22b20368aa4c3
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-09-29 13:32:45 +0300

    Java kamp

---
+++ _2.Gun/src/Calistir.java
@@ -0,0 +1,9 @@
+
+
+public class Calistir {
+    public static void main(String[] args) {
+        Deneme d=new Deneme();
+      
+
+    }
+}
---
+++ _2.Gun/src/Deneme.java
@@ -0,0 +1,17 @@
+public class Deneme {
+    static {
+        System.out.println("Statik metodun ici");
+    }
+    {
+        System.out.println("Anonim metodun ici");
+    }
+
+    public Deneme() {
+        System.out.println("Constructor ici");
+    }
+
+
+
+
+
+}
